b8b6cf54e096ba78d4a1e847ce35df996b1bc2d1
lrO-Ojl <hal0wasted@gmail.com>
2016-05-20 22:22:37 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,17 +1,3 @@
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("article button");
-
-function vidFade() {
-    vid.classList.add("stopfade");
-}
-
-vid.addEventListener('ended', function() {
-    // only functional if "loop" is removed 
-    vid.pause();
-    // to capture IE10
-    vidFade();
-});
-
 // Js only for drag the articles
 $(function() {
 	$( "article" ).draggable();
